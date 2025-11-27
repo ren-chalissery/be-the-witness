c72a75c8575242802beb4de6a03e78f3209dcd70
Ren <ren.chalissery@autodesk.com>
2025-11-27 14:26:07 +1100

    Update hero section: full-page sliding images, centered content, improved header layout

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 
@@ -9,7 +9,7 @@ import { RouterModule } from '@angular/router';
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit, OnDestroy {
   heroTitle = 'Be The Witness';
   heroSubtitle = 'THE MUSICAL THAT WILL TURN YOUR LIFE AROUND';
   
@@ -26,7 +26,39 @@ export class HomeComponent {
     { title: 'Prayer', description: 'Together we pray', route: '/prayer' }
   ];
 
-  mainImage = 'assets/images/main image.jpeg';
+  heroImages = [
+    'assets/images/main image.jpeg',
+    'assets/images/WhatsApp Image 2025-11-04 at 15.41.28.jpeg',
+    'assets/images/WhatsApp Image 2025-11-04 at 15.42.23 (1).jpeg',
+    'assets/images/WhatsApp Image 2025-11-04 at 15.42.23.jpeg',
+    'assets/images/WhatsApp Image 2025-11-04 at 15.42.24 (1).jpeg',
+    'assets/images/WhatsApp Image 2025-11-04 at 15.42.24 (2).jpeg',
+    'assets/images/WhatsApp Image 2025-11-04 at 15.42.24 (3).jpeg',
+    'assets/images/WhatsApp Image 2025-11-04 at 15.42.24.jpeg',
+    'assets/images/WhatsApp Image 2025-11-04 at 15.42.25 (1).jpeg',
+    'assets/images/WhatsApp Image 2025-11-04 at 15.42.25.jpeg'
+  ];
+
+  currentImageIndex = 0;
+  previousImageIndex = -1;
+  private imageInterval: any;
+
+  ngOnInit() {
+    this.startImageCarousel();
+    }
+
+  ngOnDestroy() {
+    if (this.imageInterval) {
+      clearInterval(this.imageInterval);
+    }
+  }
+
+  startImageCarousel() {
+    this.imageInterval = setInterval(() => {
+      this.previousImageIndex = this.currentImageIndex;
+      this.currentImageIndex = (this.currentImageIndex + 1) % this.heroImages.length;
+    }, 5000); // Change image every 5 seconds
+  }
   
   galleryImages = [
     'assets/images/WhatsApp Image 2025-11-04 at 15.41.28.jpeg',
@@ -42,7 +74,7 @@ export class HomeComponent {
 
   openImage(imageSrc: string) {
     window.open(imageSrc, '_blank');
-  }
+      }
 
   websiteUrl = 'https://www.bethewitness.com.au/';
   qrCodeUrl = `https://api.qrserver.com/v1/create-qr-code/?size=300x300&data=${encodeURIComponent(this.websiteUrl)}`;
